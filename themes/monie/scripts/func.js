809b340c2ad0aafdd91e5ef2617c0dfc636f9037
yaoyuan <yaoyuan@inchitech.com>
2024-10-23 16:20:50 +0800

    修改主题，增加front-matter封面字段:cover

--- themes/monie/scripts/func.js
+++ themes/monie/scripts/func.js
@@ -18,14 +18,22 @@ function random_bg() {
   return '/assets/images/random/' + randomNum(1, 35) + '.jpg';
 }
 // 获取文章图
-function content_img(content) {
-  const contentImg = this.url_for(random_bg());
-  if (!content) return contentImg;
+function content_img(post) {
+  // 首先检查是否有封面图
+  if (post.cover) {
+    return this.url_for(post.cover);
+  }
 
-  const img = content.match(/<img.*src=[\"'](.*)[\"']/i);
-  if (!img) return contentImg;
-  return img[1];
+  // 没有封面图，尝试从内容中提取图片
+  if (post.content) {
+    const img = post.content.match(/<img.*src=[\"'](.*)[\"']/i);
+    if (img && img[1]) return img[1];
+  }
+
+  // 没有内容图，使用默认图片
+  return this.url_for(random_bg());
 }
+
 
 // 上下翻页
 function thurning(prev = {}, next = {}) {
@@ -35,13 +43,13 @@ function thurning(prev = {}, next = {}) {
   const next_title = next.title ? next.title : '已经没有下一篇了';
 
   return `<a class="page_up" href="${this.url_for(prev_url)}"
-  style="background-image:url(${this.content_img(prev.content)})">
+  style="background-image:url(${this.content_img(prev)})">
   <div class="inner"><span>上一篇:</span>
     <p>${prev_title}</p>
   </div>
 </a>
 <a class="page_down" href="${this.url_for(next_url)}"
-style="background-image:url(${this.content_img(next.content)})">
+style="background-image:url(${this.content_img(next)})">
 <div class="inner"><span>下一篇:</span>
   <p>${next_title}</p>
 </div>
